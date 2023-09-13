23d6d0b554e9d16850f885dd98f86dd449764334
vitalij-demjanovic <vitalijdemjanovic@gmail.com>
2023-09-13 07:41:12 +0200

    login page && theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      primary: "#a94c4b",
+      secondary: "#3744b8",
+      third: "#282828",
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
-}
+};
